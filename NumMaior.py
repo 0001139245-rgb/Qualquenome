d3a1dfb0b487823475fbbb9812f4d70d336254df
Daniel r. <0001139245@senaimgaluno.com.br>
2026-04-15 15:20:05 -0300

    Add files via upload

---
+++ NumMaior.py
@@ -0,0 +1,14 @@
+
+numero = int (input("digite um numero "))
+letra = int (input("digite ,outro ,numero "))
+numero2 = ""
+
+        if (numero < numero2):
+            print(numero ," e maior que ", numero )
+        else:
+            print(numero ," e menor que ", numero2)
+
+
+
+
+
